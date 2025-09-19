39e066c68af8c8c856a5f4a1133d1d6327a7f194
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-09-19 10:49:59 -0300

    fix: adicionar suporte a randomUUID no ambiente global

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as nodeCrypto from 'crypto';
+if (!(global as any).crypto) {
+  (global as any).crypto = {
+    randomUUID: nodeCrypto.randomUUID,
+  };
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
